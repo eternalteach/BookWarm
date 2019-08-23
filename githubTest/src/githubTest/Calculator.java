c9f8a6cc7e74645a61a7911b94cfef6f960b676c
JIHYUNLIM <jihyun0552@naver.com>
2019-08-23 17:50:15 +0900

    hijihyun
    jihyunee

--- githubTest/src/githubTest/Calculator.java
+++ githubTest/src/githubTest/Calculator.java
@@ -2,7 +2,7 @@ package githubTest;
 
 public interface Calculator {
 
-
+// jihyuneee jjang
 	public double add(double first, double second); // KY
 	public double div(double first, double second); // JH
 	public double min(double first, double second); // TH
