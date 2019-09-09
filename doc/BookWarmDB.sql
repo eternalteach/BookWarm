e7f3378749f93cef36ea15efc08e762f72e34084
cjtmarglike <50134611+cjtmarglike@users.noreply.github.com>
2019-09-09 17:24:33 +0900

    Update BookWarmDB.sql
    190909 
    - 각종 no(10,0)으로 통일
    - user_id, isbn not null
    - content 크기(댓글 200, 본문 2000) 수정
    - title 크기 200
    - 경로 길이 100
    - 그외 각종 not null

--- doc/BookWarmDB.sql
+++ doc/BookWarmDB.sql
@@ -4,24 +4,13 @@
 create sequence seq_logBoard;
 create sequence seq_logingBoard;
 
--- logBoard 보드 생성
-create table log_board(
-write_no number(10,0) PRIMARY key,
-user_id varchar2(50) not null,
-write_written_date date default sysdate,
-isbn VARCHAR2(20) not null,
-write_buy_date date ,
-read_amount number(10,0),
-read_stop number(10,0),
-read_star number(1,0)
-);
 
 -- logingBoard 보드 생성(기범)
 create table loging_board(
-isbn varchar2(20),
+isbn varchar2(20) not null,
 write_no number(10,0) PRIMARY key,
-start_page number(4,0) default 1 not null,
-end_page number(4,0) not null,
+start_page number(10,0) default 1 not null,
+end_page number(10,0) not null,
 start_date date default sysdate,
 end_date varchar2(10) default 'false');
 
@@ -30,13 +19,13 @@ create sequence review_board_seq;
 --독서감상 테이블 (김해랑)
 create table review_board(
 review_no  NUMBER (10,0) primary key,
-user_id VARCHAR2(20),
+user_id VARCHAR2(20) not null,
 review_written_date date default sysdate,
 review_modify_date date default sysdate,
-isbn varchar2(20),
-review_ref number(4,0),
-review_title varchar2(200),
-review_content varchar2(2000),
+isbn varchar2(20) not null,
+review_ref number(10,0),
+review_title varchar2(200) not null,
+review_content varchar2(2000) not null,
 review_open varchar2(1) default 'N');
 
 ----댓글
@@ -44,22 +33,22 @@ create sequence review_comment_seq;
 --
 ----댓글 테이블 (김해랑)(9/5 review_reply -> review_comment로 수정)
 create table review_comment(
-    review_cmt_no number(10),
-    review_no number(10),
-    user_id varchar2(20),
-    review_cmt_content varchar2(500),
-    review_cmt_written_date date,
-    review_cmt_modified_date date,
+    review_cmt_no number(10,0),
+    review_no number(10,0) not null,
+    user_id varchar2(20) not null,
+    review_cmt_content varchar2(200) not null,
+    review_cmt_written_date date default sysdate,
+    review_cmt_modified_date date default sysdate,
     CONSTRAINT pk_review_comment primary key (review_cmt_no)
 );
 create index idx_comment on review_comment(review_no desc, review_cmt_no asc);
 
 --파일첨부(김해랑)
 create table review_attach(
-    uuid varchar2(100) not null,
+    uuid varchar2(100),
     uploadPath varchar2(200) not null,
     fileName varchar2(100) not null,
-    review_no number(10)
+    review_no number(10,0)
 );
 
 alter table review_attach add constraint pk_attach primary key(uuid);
@@ -68,30 +57,28 @@ alter table review_attach add constraint fk_review_attach foreign key(review_no)
 
 --해쉬태그 시퀀스
 create sequence hashtag_seq;
---해쉬태그 테이블 (김해랑 사용 예정)
+--해쉬태그 테이블 (김해랑 사용 예정 - 보류)
 create table hashtag(
 review_no number(10,0),
 tag_name varchar2(50) not null
 );
-
-
 
 ------------------------------------------------------------
 
 -------------------- create community board --------------------
 --community board (박기범) (9/6 컬럼 크기, pk이름 수정)
 create table community_board(
-comm_no NUMBER(10),
-user_id VARCHAR2(20),
+comm_no NUMBER(10,0),
+user_id VARCHAR2(20) not null,
 comm_subject varchar2(30),
-comm_title VARCHAR2(200),
-comm_content VARCHAR2(3000),
+comm_title VARCHAR2(200) not null,
+comm_content VARCHAR2(2000) not null,
 comm_written_time DATE DEFAULT SYSDATE,
 comm_modify_time DATE DEFAULT SYSDATE,
-comm_clicked NUMBER(10) DEFAULT 0,
-comm_group NUMBER(10),
-comm_step NUMBER(4),
-comm_indent NUMBER(4)
+comm_clicked NUMBER(10,0) DEFAULT 0,
+comm_group NUMBER(10,0),
+comm_step NUMBER(10,0),
+comm_indent NUMBER(10,0)
 );
 
 -- pk설정, comm의 pk를 comm_pk_board로 설정
@@ -103,16 +90,16 @@ create sequence community_board_comment_seq;
 
 -- community_board_comment table (박기범)
 create table community_board_comment(
-comm_no number(10),
-comm_cmt_no NUMBER(10),
-user_id VARCHAR2(20),
-comm_cmt_content VARCHAR2(3000),
+comm_no number(10,0),
+comm_cmt_no NUMBER(10,0),
+user_id VARCHAR2(20) not null,
+comm_cmt_content VARCHAR2(2000) not null,
 comm_cmt_written_time DATE DEFAULT SYSDATE,
 comm_cmt_modify_time DATE DEFAULT SYSDATE,
 comm_cmt_deleted char(1) default 'n',
-comm_cmt_group NUMBER(10),
-comm_cmt_step NUMBER(4),
-comm_cmt_indent NUMBER(4));
+comm_cmt_group NUMBER(10,0),
+comm_cmt_step NUMBER(10,0),
+comm_cmt_indent NUMBER(10,0));
 
 -- community Board Comment Index
 create index idx_comm_cmt on community_board_comment (comm_no desc, comm_cmt_no asc);
@@ -123,24 +110,19 @@ ALTER TABLE community_board_comment ADD CONSTRAINT comm_cmt_pk_board primary KEY
                        ---------------------------------------
 
 -------------------- book --------------------
---book ISBNimg
-create table book_cover(
-isbn varchar2(20),
-img_src VARCHAR2(1000)
-);
 
 --book
 create table book(
 isbn varchar2(20) primary key,
-book_title varchar2(50) not null,
-writer_no number(20) ,
-translator_no number(20),
-publisher_no number(20),
-book_tot_page number(5) ,
+book_title varchar2(200) not null,
+writer_name varchar2(100)  not null,
+translator_name varchar2(100),
+publisher_name varchar2(100) not null,
+book_tot_page number(10,0) default 1000,
 book_published_date DATE,
-book_price number(7) not null,
-book_price_for_sale number(7),
-book_stock number(10) not null,
+book_price number(10,0),
+book_price_for_sale number(10,0),
+book_stock number(10,0) not null,
 book_intro varchar2(500),
 book_idx varchar2(500),
 book_story varchar2(500),
@@ -148,39 +130,7 @@ book_img varchar2(100)
 );
 ---------------------------------------------
 
---------------- 작가 ---------------
-create table writer(
-    writer_no number(10,0), 
-    writer_name varchar(50),
-    writer_info varchar(50)
-    );
----------------------------------------------
---------------- 역자 ---------------
-create table translator(
-translator_no number(20) primary Key,
-translator_name varchar2(20) not null,
-translator_info varchar2(200)
-);
----------------------------------------------
---------------- 출판사 ---------------
-create sequence publisher_seq;
-create table publisher(
-    publisher_no number(20,0) primary key,
-    publisher_name varchar(50) not null,
-    publisher_phone number(20,0),
-    publisher_addr varchar(50) 
-);
 
----------------------------------------------
-
-
-
---------------- 카테고리 ---------------
-create table category (
-isbn varchar2(20) not null,
-genre varchar2(20) not null
-);
----------------------------------------------
 --------------- 책속 한줄 글귀 ---------------
 create table book_clause(
 isbn varchar2(20) not null,
@@ -188,74 +138,52 @@ clause varchar2(300) not null
 );
 ---------------------------------------------
 
---결제
-create table pay(
-pay_no varchar2(20) primary key,
-pay_way varchar2(20) not null,
-pay_total number(10,0) not null,
-pay_refund_account varchar2(50) not null
-);
-
---------------- cart ---------------(임지현)
-create table cart(
-cart_no number(20) primary key,
-user_id varchar2(20), -- fk 설정하기
-isbn varchar2(20), --fk설정하기
-cart_cnt number(20)
-);
-
----------------------------------------------
 
 --------------- user ---------------(임지현)
 create table user_info(
 user_id varchar2(20) primary key,
 user_pw varchar2(64) not null,
-user_nickname varchar2(20),
-user_name varchar2(20),
-user_bday date,
-user_sex char(1),
-user_phone varchar2(20),
-user_mail varchar2(50),
-user_zipcode number(10,0),
-user_addr varchar2(100),
+user_nickname varchar2(20) not null,
+user_name varchar2(20) not null,
+user_bday date not null,
+user_sex char(1) not null,
+user_phone varchar2(20) not null,
+user_mail varchar2(50) not null,
+user_zipcode number(10,0) not null,
+user_addr varchar2(100) not null,
 user_addr_detail varchar2(100),
-user_join_date date,
-user_level varchar2(10),
-user_tot_price number(30,0),
-user_point number(20,0)
+user_join_date date default sysdate,
+user_level varchar2(10) default 'egg',
+user_tot_price number(15,0),
+user_point number(10,0)
 );
 
 --친구
 create table friend(
-userid varchar2(20),
-friend_id varchar2(20));
+user_id varchar2(20) not null,
+friend_id varchar2(20) not null);
 
 --메시지 기능
 create sequence msg_seq;
 create table msg_table(
-msg_no number(20,0),
-msg_send_id varchar2(20),
-msg_get_id varchar2(20),
-msg_title varchar2(20),
-msg_content varchar2(500),
+msg_no number(10,0) primary key,
+msg_send_id varchar2(20) not null,
+msg_get_id varchar2(20) not null,
+msg_title varchar2(200) not null,
+msg_content varchar2(2000) not null,
 msg_send_time date default sysdate,
 msg_read_time date default sysdate
 );
 
 --내 서재
 create table library(
-userid varchar2(20),
-list_no number(20,0)
-list_type varchar2(10),
-isbn varchar2(20),
+user_id varchar2(20) not null,
+list_no number(10,0) default 1,
+list_img_src varchar2(100),
+list_type varchar2(10) default 'basic',
+isbn varchar2(20) not null,
 list_added_date date default sysdate
 );
-
-
---회원등급 (이건 왜 필요하지?)
-create table user_level(
-user_level varchar2(10) not null,
-level_qualification(100) not null);
 
 --쿠폰
 create table coupon(
@@ -270,8 +198,8 @@ coupon_validate date not null
 
 --보유 쿠폰
 create table coupon_no(
-coupon_no varchar2(15),
-userid varchar2(20),
+coupon_no varchar2(15) not null,
+user_id varchar2(20) not null,
 coupon_available char(1) default 'y'
 );
 
@@ -279,25 +207,52 @@ coupon_available char(1) default 'y'
 create sequence order_seq;
 create table order(
 order_no varchar2(20) primary key,
-userid varchar2(20) not null,
+user_id varchar2(20) not null,
 order_date date not null,
-isbn varchar2(20),
+isbn varchar2(20) not null,
 order_cnt number(10,0) not null,
 order_tot number(10,0) not null,
-order_start_date date,
-order_pay_date date,
+order_start_date date not null,
+order_pay_date date not null,
 coupon_no varchar2(20),
 post_no varchar2(20),
 pay_no varchar2(20));
+
+alter table order add constraint fk_pay_no foreign key(pay_no)
+    references pay(pay_no);
 
 
 --주문내역
 create table orderview(
 order_no varchar2(20) primary key,
-userid varchar2(20),
+user_id varchar2(20) not null,
 order_status varchar2(10)
 );
 
+
+--결제
+create table pay(
+pay_no varchar2(20) primary key,
+pay_way varchar2(20) not null,
+pay_total number(10,0) not null,
+pay_refund_account varchar2(50) not null
+);
+
+--------------- cart ---------------(임지현)
+create table cart(
+cart_no number(20) primary key,
+user_id varchar2(20) not null, -- fk 설정하기
+isbn varchar2(20) not null, --fk설정하기
+cart_cnt number(20)
+);
+
+alter table cart add constraint fk_user_id foreign key(user_id)
+    references user_info(user_id);
+
+alter table cart add constraint fk_isbn foreign key(isbn)
+    references book(isbn);
+
+---------------------------------------------
 
 --------------- 책으로 답하는 메모보드 ---------------
 -- 메모보드 시퀀스 생성
@@ -305,33 +260,27 @@ create sequence memo_board_seq;
 create sequence book_reply_seq;
 
 CREATE TABLE memo_board(
-memo_no number(20) primary key,
-user_id varchar2(20),
-memo_title_b varchar2(100),
-memo_content_b varchar(3000),
+memo_no number(10,0) primary key,
+user_id varchar2(20) not null,
+memo_content_b varchar(2000) not null,
 memo_written_time date default sysdate,
 memo_modify_time date default sysdate
 );
 
 --메모보드 책 댓글
 CREATE TABLE book_reply(
-book_re_no number(20) primary key,
-memo_no number(20), -- fk 설정하기
-user_id varchar2(20),
-isbn varchar2(20),
-book_re_content varchar(500),
-book_re_written_time date default sysdate,
-book_re_modify_time date default sysdate,
-book_re_like number(10) default 0
+book_cmt_no number(10,0) primary key,
+memo_no number(10,0), -- fk 설정하기
+user_id varchar2(20) not null,
+isbn varchar2(20) not null,
+book_cmt_content varchar(200) not null,
+book_cmt_written_time date default sysdate,
+book_cmt_modify_time date default sysdate,
+book_cmt_like number(10,0) default 0
 );
 -- book_reply memo_no 외래키 설정
 ALTER TABLE book_reply ADD CONSTRAINTS memo_no FOREIGN KEY(memo_no) REFERENCES memo_board;
 ------------------------------------------------------------------------------------------
-
-
-
-
-
 
 
 ------------------------------ MD추천 (큐레이션) ------------------------------
@@ -340,14 +289,8 @@ create sequence curation_seq;
 create table curation (
 curation_no number(10) primary key,
 curation_name varchar2(20) not null,
-isbn varchar2(20));
+isbn varchar2(20) not null);
 
 ---------------------------------------------------------------------------
-
-
-
-
-
-
 
 
