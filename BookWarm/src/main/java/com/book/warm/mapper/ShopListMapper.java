354906b584239a02c797528f52418f169f86c77c
cjtmarglike <50134611+cjtmarglike@users.noreply.github.com>
2019-09-10 21:57:33 +0900

    190910_merge 후반 작업
    master와 병합 후 vo, controller, mapper 등 수정 ing

--- BookWarm/src/main/java/com/book/warm/mapper/ShopListMapper.java
+++ BookWarm/src/main/java/com/book/warm/mapper/ShopListMapper.java
@@ -3,28 +3,9 @@ package com.book.warm.mapper;
 import java.util.List;
 
 import com.book.warm.page.Criteria;
-import com.book.warm.vo.ShopListBoardVO;
 
 public interface ShopListMapper {
 	
-	////isbn ������ �ҷ�����
-	public List<ShopListBoardVO> shoplist();
-	
-	//�����(��������)������ �ҷ�����
-	public List<ShopListBoardVO> shoplist2();
-	
-	//å �������� �������� �̵�
-	public ShopListBoardVO bookdetail(String isbn);
-	
-	//å�� ���� �帣 ��������
-	public List<ShopListBoardVO> bookgenre(String isbn);
-	
-	//�����̸��� ���� å �ҷ�����
-	public List<ShopListBoardVO> bookwriter(String writer_name);
-	
-	//����¡ ó��
-	public List<ShopListBoardVO> shoplistpage(Criteria criteria);
-	
-	public int gettotalcount(Criteria criteria);
+
 	
 }
